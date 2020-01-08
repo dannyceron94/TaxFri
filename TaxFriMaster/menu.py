3a62361b1bfff7201ff53aeaf1c140953d32646f
dannyceron94 <dannyceron94@gmail.com>
2020-01-08 00:41:35 -0800

    sa

--- TaxFriMaster/menu.py
+++ TaxFriMaster/menu.py
@@ -1,6 +1,5 @@
 """
 created on Jan 26 2019
-modified on Jan 1 2020
 @author: DANNY CERON
  """
 from User import User
@@ -8,21 +7,18 @@ from Questions import Questions
 from DateClass import DateClass
 class menu(User):
     
+    
     def new_user(self):
         
         user_input = None
-        #creates a user instance.
         main_user = User()
         item_date = DateClass()
-        #checks if there is there existing data files.
         checker = main_user.checkFile()
-        #creates new data file if no datafile is found.
         if(checker == False):
             name = main_user.string_question("What is your full name?")
             birth_day = main_user.string_question("enter your birth date (ex: 01/01/2000):")
             main_user.__init__(name, birth_day)
             main_user.createFile()
-            
             self.new_user()
             #set my account can be used, recently added that method on User class
             
@@ -31,18 +27,11 @@ class menu(User):
                 
                 user_input = self.menu_display()
                 if(user_input == 1):
-                    #printing all the data stored in the file.
-                    main_user.readData()
-                    input("\nPress enter to continue")
+                    print("figuring out a date structure for it")
                 elif(user_input == 2):
-                    main_user.add_transaction()                
-                    #add the total
-                elif(user_input == 3):
-                    main_user.deleteItem()
-
-                elif(user_input == 4):
-                    main_user.userInfo()
-                    input("\nPress enter to continue")
+                    item, price, category = main_user.AskForItem()
+                    
+                    main_user.add_item(item, price, category, item_date.__str__(), note = None)
         else:
             print("Something went wrong!")
     def menu_display(self):
